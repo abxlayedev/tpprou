eac59111c65d6327267252e46f69e72f205578ae
abxlayedev <67095076+abxlayedev@users.noreply.github.com>
2020-09-22 16:13:22 +0200

    first commit

---
+++ TPmariage/src/Conjoint.java
@@ -0,0 +1,25 @@
+
+public class Conjoint extends Personne {
+	
+	private String name;
+	private boolean married;
+
+	public boolean isMarried() {
+		return married;
+	}
+
+	public void setMarried(boolean married) {
+		this.married = married;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
